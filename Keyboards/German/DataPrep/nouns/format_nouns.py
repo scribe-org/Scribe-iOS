14bb09c93693401eb28425ec651be635bcf15e16
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-11-01 14:28:20 +0100

    Update nouns query to include proper nouns and formatting .py file to account for all variants

--- Keyboards/German/DataPrep/nouns/format_nouns.py
+++ Keyboards/German/DataPrep/nouns/format_nouns.py
@@ -2,7 +2,7 @@
 Nouns Format
 ------------
 
-Formats the nouns queried from Wikidata using nounsQueried.sparql
+Formats the nouns queried from Wikidata using queryNouns.sparql.
 """
 
 import json
@@ -27,62 +27,103 @@ nouns_formatted = {}
 
 for noun_vals in nouns_list:
     if "singular" in noun_vals.keys():
-        if "plural" in noun_vals.keys() and "gender" in noun_vals.keys():
-            if (
-                noun_vals["singular"] in nouns_formatted
-                and nouns_formatted[noun_vals["singular"]]["form"] != "noForm"
-            ):
-                # WIP: finding a solution for words with more than one plural.
-                # Add the form only if it's not yet present.
-                if (
-                    map_genders(noun_vals["gender"])
-                    not in nouns_formatted[noun_vals["singular"]]["form"]
-                ):
-                    nouns_formatted[noun_vals["singular"]] = {
-                        "plural": noun_vals["plural"],
-                        "form": nouns_formatted[noun_vals["singular"]]["form"]
-                        + "/"
-                        + map_genders(noun_vals["gender"]),
-                    }
-            else:
+        if noun_vals["singular"] not in nouns_formatted:
+            # Get plural and gender.
+            if "plural" in noun_vals.keys() and "gender" in noun_vals.keys():
                 nouns_formatted[noun_vals["singular"]] = {
                     "plural": noun_vals["plural"],
                     "form": map_genders(noun_vals["gender"]),
                 }
 
-                if (
-                    noun_vals["plural"] in nouns_formatted
-                    and "PL" not in nouns_formatted[noun_vals["singular"]]["form"]
-                ):
-                    nouns_formatted[noun_vals["plural"]] = {
-                        "plural": noun_vals["plural"],
-                        "form": nouns_formatted[noun_vals["singular"]]["form"]
-                        + "/"
-                        + "PL",
-                    }
+                # Assign plural as a new entry after checking if it's its own plural.
+                if noun_vals["plural"] not in nouns_formatted:
+                    if noun_vals["singular"] != noun_vals["plural"]:
+                        nouns_formatted[noun_vals["plural"]] = {
+                            "plural": "isPlural",
+                            "form": "PL",
+                        }
+                    else:
+                        nouns_formatted[noun_vals["plural"]] = {
+                            "plural": noun_vals["plural"],
+                            "form": "PL",
+                        }
+                else:
+                    # Mark plural as a possible form if it isn't already.
+                    if (
+                        "PL" not in nouns_formatted[noun_vals["plural"]]["form"]
+                        and nouns_formatted[noun_vals["plural"]]["form"] != "noForm"
+                    ):
+                        nouns_formatted[noun_vals["plural"]]["form"] = (
+                            nouns_formatted[noun_vals["plural"]]["form"] + "/" + "PL"
+                        )
+                    elif nouns_formatted[noun_vals["plural"]]["form"] == "noForm":
+                        nouns_formatted[noun_vals["plural"]]["form"] = "PL"
+                    # Assign itself as a plural if possible (maybe wasn't for prior versions).
+                    if noun_vals["singular"] == noun_vals["plural"]:
+                        nouns_formatted[noun_vals["plural"]]["plural"] = noun_vals[
+                            "plural"
+                        ]
 
-        elif "plural" in noun_vals.keys():
-            nouns_formatted[noun_vals["singular"]] = {
-                "plural": noun_vals["plural"],
-                "form": "noForm",
-            }
+            # Get plural and assign it as a noun.
+            elif "plural" in noun_vals.keys() and "gender" not in noun_vals.keys():
+                nouns_formatted[noun_vals["singular"]] = {
+                    "plural": noun_vals["plural"],
+                    "form": "noForm",
+                }
 
-            nouns_formatted[noun_vals["singular"]["plural"]] = {
+                # Assign plural as a new entry after checking if it's its own plural.
+                if noun_vals["plural"] not in nouns_formatted:
+                    if noun_vals["singular"] != noun_vals["plural"]:
+                        nouns_formatted[noun_vals["plural"]] = {
+                            "plural": "isPlural",
+                            "form": "PL",
+                        }
+                    else:
+                        nouns_formatted[noun_vals["plural"]] = {
+                            "plural": noun_vals["plural"],
+                            "form": "PL",
+                        }
+                else:
+                    # Mark plural as a possible form if it isn't already.
+                    if (
+                        "PL" not in nouns_formatted[noun_vals["plural"]]["form"]
+                        and nouns_formatted[noun_vals["plural"]]["form"] != "noForm"
+                    ):
+                        nouns_formatted[noun_vals["plural"]]["form"] = (
+                            nouns_formatted[noun_vals["plural"]]["form"] + "/" + "PL"
+                        )
+                    elif nouns_formatted[noun_vals["plural"]]["form"] == "noForm":
+                        nouns_formatted[noun_vals["plural"]]["form"] = "PL"
+                    # Assign itself as a plural if possible (maybe wasn't for prior versions).
+                    if noun_vals["singular"] == noun_vals["plural"]:
+                        nouns_formatted[noun_vals["plural"]]["plural"] = noun_vals[
+                            "plural"
+                        ]
+
+            elif "plural" not in noun_vals.keys() and "gender" in noun_vals.keys():
+                nouns_formatted[noun_vals["singular"]] = {
+                    "plural": "noPlural",
+                    "form": map_genders(noun_vals["gender"]),
+                }
+
+    elif "plural" in noun_vals.keys():
+        if noun_vals["plural"] not in nouns_formatted:
+            nouns_formatted[noun_vals["plural"]] = {
                 "plural": "isPlural",
                 "form": "PL",
             }
+        else:
+            # Mark plural as a possible form if it isn't already.
+            if (
+                "PL" not in nouns_formatted[noun_vals["plural"]]["form"]
+                and nouns_formatted[noun_vals["plural"]]["form"] != "noForm"
+            ):
+                nouns_formatted[noun_vals["plural"]]["form"] = (
+                    nouns_formatted[noun_vals["plural"]]["form"] + "/" + "PL"
+                )
+            elif nouns_formatted[noun_vals["plural"]]["form"] == "noForm":
+                nouns_formatted[noun_vals["plural"]]["form"] = "PL"
 
-        elif "gender" in noun_vals.keys():
-            nouns_formatted[noun_vals["singular"]] = {
-                "plural": "noPlural",
-                "form": map_genders(noun_vals["gender"]),
-            }
-
-    elif "plural" in noun_vals.keys():
-        nouns_formatted[noun_vals["plural"]] = {
-            "plural": "isPlural",
-            "form": "PL",
-        }
 
 with open("../../nouns.json", "w", encoding="utf-8") as f:
     json.dump(nouns_formatted, f, ensure_ascii=False, indent=2)
