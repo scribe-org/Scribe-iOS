7e35d76d107c3479292a68ba15acfb70e20155ef
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-11-13 18:47:00 +0100

    Fixing case names so that they're displayed in German to the user

--- Keyboards/German/DataPrep/prepositions/format_prepositions.py
+++ Keyboards/German/DataPrep/prepositions/format_prepositions.py
@@ -11,19 +11,29 @@ import json
 with open("prepositionsQueried.json") as f:
     prepositions_list = json.load(f)
 
+
+def convert_cases(case):
+    if case == "accusative":
+        return "Akkusativ"
+    if case == "dative":
+        return "Dativ"
+    if case == "genitive":
+        return "Genitiv"
+
+
 prepositions_formatted = {}
 
 for prep_vals in prepositions_list:
     if "preposition" in prep_vals.keys():
         if "case" in prep_vals.keys():
             if prep_vals["preposition"] not in prepositions_formatted:
-                prepositions_formatted[prep_vals["preposition"]] = prep_vals[
-                    "case"
-                ].split(" case")[0]
+                prepositions_formatted[prep_vals["preposition"]] = convert_cases(
+                    prep_vals["case"].split(" case")[0]
+                )``
 
             else:
-                prepositions_formatted[prep_vals["preposition"]] += (
-                    "/" + prep_vals["case"].split(" case")[0]
+                prepositions_formatted[prep_vals["preposition"]] += "/" + convert_cases(
+                    prep_vals["case"].split(" case")[0]
                 )
 
         elif "case" not in prep_vals.keys():
