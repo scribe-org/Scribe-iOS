07a6d33531cd472f6592d9fe6681ff7834183b30
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-07-02 16:18:14 +0530

    feat: assiging placeholder variables in Spanish keyboard

--- Keyboards/LanguageKeyboards/Spanish/ESInterfaceVariables.swift
+++ Keyboards/LanguageKeyboards/Spanish/ESInterfaceVariables.swift
@@ -111,15 +111,21 @@ func setESKeyboardLayout() {
   invalidCommandMsg = "No en Wikidata"
 
   translateKeyLbl = "Traducir"
+  translatePlaceholder = "Ingrese la palabra"
   translatePrompt = commandPromptSpacing + "es -› \(getControllerLanguageAbbr()): "
   translatePromptAndCursor = translatePrompt + commandCursor
+  translatePromptAndPlaceholder = translatePromptAndCursor + translatePlaceholder
 
   conjugateKeyLbl = "Conjugar"
+  conjugatePlaceholder = "Ingresar verbo"
   conjugatePrompt = commandPromptSpacing + "Conjugar: "
   conjugatePromptAndCursor = conjugatePrompt + commandCursor
+  conjugatePromptAndPlaceholder = conjugatePromptAndCursor + conjugatePlaceholder
 
   pluralKeyLbl = "Plural"
+  pluralPlaceholder = "Ingrese sustantivo"
   pluralPrompt = commandPromptSpacing + "Plural: "
   pluralPromptAndCursor = pluralPrompt + commandCursor
+  pluralPromptAndPlaceholder = pluralPromptAndCursor + pluralPlaceholder
   isAlreadyPluralMessage = "Ya en plural"
 }
