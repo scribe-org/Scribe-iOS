0000c326c2b00b203667d8d9e4e2f34737a72c23
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2023-06-02 17:47:33 +0530

    getting scroll view to work in about page

--- Scribe/AboutSectionData.swift
+++ Scribe/AboutSectionData.swift
@@ -12,6 +12,7 @@ struct AboutSectionData {
   struct DataModel {
     let title: String
     let imageName: String
+    let showsNewScreen: Bool
   }
   
   enum AboutSections {
@@ -24,22 +25,22 @@ struct AboutSectionData {
     switch section {
     case .community:
       return [
-        DataModel(title: "See the code on GitHub", imageName: "github"),
-        DataModel(title: "Chat with the team on Matrix", imageName: "matrix"),
-        DataModel(title: "Wikimedia and Scribe", imageName: "wikimedia"),
-        DataModel(title: "Share Scribe", imageName: "square.and.arrow.up")
+        DataModel(title: "See the code on GitHub", imageName: "github", showsNewScreen: false),
+        DataModel(title: "Chat with the team on Matrix", imageName: "matrix", showsNewScreen: false),
+        DataModel(title: "Wikimedia and Scribe", imageName: "wikimedia", showsNewScreen: true),
+        DataModel(title: "Share Scribe", imageName: "square.and.arrow.up", showsNewScreen: false)
       ]
     case .feedbackAndSupport:
       return [
-        DataModel(title: "Rate Scribe", imageName: "star"),
-        DataModel(title: "Report a bug", imageName: "ant"),
-        DataModel(title: "Send us an email", imageName: "envelope"),
-        DataModel(title: "Reset app hints", imageName: "lightbulb")
+        DataModel(title: "Rate Scribe", imageName: "star", showsNewScreen: false),
+        DataModel(title: "Report a bug", imageName: "ant", showsNewScreen: false),
+        DataModel(title: "Send us an email", imageName: "envelope", showsNewScreen: false),
+        DataModel(title: "Reset app hints", imageName: "lightbulb", showsNewScreen: false)
       ]
     case .legal:
       return [
-        DataModel(title: "Privacy policy", imageName: "lock.shield"),
-        DataModel(title: "Third-party licenses", imageName: "thirdPartyLicenses")
+        DataModel(title: "Privacy policy", imageName: "lock.shield", showsNewScreen: true),
+        DataModel(title: "Third-party licenses", imageName: "thirdPartyLicenses", showsNewScreen: true)
       ]
     }
   }
