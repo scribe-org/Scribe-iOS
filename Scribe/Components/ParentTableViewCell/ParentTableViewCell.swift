a25c0bf716bb9cb45e766dab1fa84c47b6c9cfaa
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2023-06-06 19:11:08 +0530

    fix: increasing cell height and disabling scroll inside child tables

--- Scribe/Components/ParentTableViewCell/ParentTableViewCell.swift
+++ Scribe/Components/ParentTableViewCell/ParentTableViewCell.swift
@@ -24,6 +24,7 @@ class ParentTableViewCell: UITableViewCell {
     innerTable.dataSource = self
     innerTable.delegate = self
     innerTable.rowHeight = UITableView.automaticDimension
+    innerTable.isScrollEnabled = false
     innerTable.reloadData()
     
     setContainerViewUI()
