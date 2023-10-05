1d21e116ea827156808aa6be018a6ccdf6f91215
stilianb <stilianb@gmail.com>
2023-10-05 12:02:17 -0600

    deleted extra delete keys for both spanish and english

--- Keyboards/LanguageKeyboards/English/ENInterfaceVariables.swift
+++ Keyboards/LanguageKeyboards/English/ENInterfaceVariables.swift
@@ -104,7 +104,6 @@ func getENKeys() {
 
     // If the iPad is too small to have a numbers row.
     letterKeys.removeFirst(1)
-    letterKeys[0].append("delete")
 
     allKeys = Array(letterKeys.joined()) + Array(numberKeys.joined()) + Array(symbolKeys.joined())
 
