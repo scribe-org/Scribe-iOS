6987fc8bd18ca2faf2a41f955095874d223eb940
Damien Rivet <damien.rivet@gmail.com>
2023-11-14 16:14:00 +0100

    [#330] Refactor cells and tableviews
    Add a base table view controller to implement similar logic between screens.

--- Scribe/SettingsTab/SettingsViewController.swift
+++ Scribe/SettingsTab/SettingsViewController.swift
@@ -1,10 +1,23 @@
 //
-//  SettingsViewController.swift
+//  Copyright (C) 2023 Scribe
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+final class SettingsViewController: UIViewController {
   @IBOutlet var footerFrame: UIView!
   @IBOutlet var footerButton: UIButton!
 
@@ -15,17 +28,15 @@ class SettingsViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    title = "Settings"
-    navigationItem.backButtonTitle = "Settings"
+    title = NSLocalizedString("settings.title", comment: "The title for the settings screen")
+    navigationItem.backButtonTitle = NSLocalizedString("settings.title.backButton", comment: "The back button's title for the settings screen")
 
-    let nib = UINib(nibName: "ParentTableViewCell", bundle: nil)
-    parentTable.register(nib, forCellReuseIdentifier: "ParentTableViewCell")
+    parentTable.register(UINib(nibName: "InfoChildTableViewCell", bundle: nil), forCellReuseIdentifier: InfoChildTableViewCell.reuseIdentifier)
 
     parentTable.dataSource = self
     parentTable.delegate = self
-
-    parentTable.separatorStyle = .none
     parentTable.backgroundColor = .clear
+    parentTable.sectionHeaderHeight = 32
 
     setFooterButtonView()
 
@@ -71,20 +82,86 @@ class SettingsViewController: UIViewController {
 }
 
 extension SettingsViewController: UITableViewDataSource {
-  func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
-    return tableData.count
+
+  func numberOfSections(in tableView: UITableView) -> Int {
+    tableData.count
+  }
+
+  func tableView(_: UITableView, numberOfRowsInSection section: Int) -> Int {
+    tableData[section].section.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ParentTableViewCell", for: indexPath) as! ParentTableViewCell
+    let cell = tableView.dequeueReusableCell(withIdentifier: InfoChildTableViewCell.reuseIdentifier, for: indexPath) as! InfoChildTableViewCell
 
-    cell.configureCell(for: tableData[indexPath.row])
+    let section = tableData[indexPath.section]
+    let setting = section.section[indexPath.row]
 
-    cell.backgroundColor = .clear
-    cell.selectionStyle = .none
+    cell.configureCell(for: setting)
 
     return cell
   }
 }
 
-extension SettingsViewController: UITableViewDelegate {}
+extension SettingsViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      let tableSection = tableData[indexPath.section]
+      let section = tableSection.section[indexPath.row]
+
+        switch section.sectionState {
+        case .specificLang:
+          if let viewController = storyboard?.instantiateViewController(identifier: "TableViewTemplateViewController") as? TableViewTemplateViewController {
+            // Copy base settings
+            var data = SettingsTableData.languageSettingsData
+
+            // Languages where we can disable accent keys.
+            let accentKeyLanguages: [String] = ["Swedish", "German", "Spanish"]
+            let accentKeyOptionIndex = SettingsTableData.languageSettingsData[0].section.firstIndex(where: {
+              s in s.sectionTitle.elementsEqual("Disable accent characters")
+            }) ?? -1
+
+            if accentKeyLanguages.firstIndex(of: section.sectionTitle) == nil && accentKeyOptionIndex != -1 {
+              // As there are no accent keys we can remove the `Disable accent characters` option.
+              let accentKeySettings = data[0].section.remove(at: accentKeyOptionIndex)
+              print(accentKeySettings)
+            } else if accentKeyLanguages.firstIndex(of: section.sectionTitle) != nil && accentKeyOptionIndex == -1 {
+              data[0].section.insert(Section(
+                sectionTitle: "Disable accent characters",
+                imageString: "info.circle",
+                hasToggle: true,
+                sectionState: .none(.toggleAccentCharacters)
+              ), at: 1)
+            }
+
+            viewController.configureTable(for: data, parentSection: section)
+
+            navigationController?.pushViewController(viewController, animated: true)
+          }
+
+        default: break
+        }
+
+      if let selectedIndexPath = tableView.indexPathForSelectedRow {
+        tableView.deselectRow(at: selectedIndexPath, animated: false)
+      }
+    }
+
+  func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    let headerView: UIView
+
+    if let reusableHeaderView = tableView.headerView(forSection: section) {
+      headerView = reusableHeaderView
+    } else {
+      headerView = UIView(frame: CGRect(x: 0, y: 0, width: parentTable.bounds.width, height: 32))
+    }
+
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: headerView.bounds.width, height: 32))
+    label.text = tableData[section].headingTitle
+    label.font = UIFont.preferredFont(forTextStyle: .headline)
+    label.textColor = .black
+    headerView.addSubview(label)
+
+    return headerView
+  }
+}
