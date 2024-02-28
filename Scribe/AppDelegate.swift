ecfac76152f46d749bbbe980f7c754feccf14a9e
Jag-Marcel <marcelmehl123@gmail.com>
2024-02-28 20:00:17 +0100

    Added copyright notice to all .swift files
    Added a generic copyright notice to all .swift files in the directory, with a short description for each file.

--- Scribe/AppDelegate.swift
+++ Scribe/AppDelegate.swift
@@ -1,8 +1,22 @@
-//
-//  AppDelegate.swift
-//
-//  A class of methods to manage Scribe's behaviors.
-//
+/**
+ * Class of methods to manage Scribe's behaviors
+ *
+ *
+ * Copyright (C) 2023 Scribe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import CoreData
 import UIKit
