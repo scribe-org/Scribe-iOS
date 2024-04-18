7feb88ceb23ba2744f8af2d9590d70aeb3465591
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2024-04-18 12:46:18 +0530

    temporarily commenting unreferenced files

--- Scribe/InstallationTab/DownloadDataTable.swift
+++ Scribe/InstallationTab/DownloadDataTable.swift
@@ -7,27 +7,27 @@
 
 import Foundation
 
-struct DownloadDataTable {
-  static var downloadDataTable = [
-    ParentTableCellModel(
-      headingTitle: "Data updates",
-      section: [
-        Section(sectionTitle: "Check for new data", imageString: "checkmark.icloud", hasToggle: false, sectionState: .checkData),
-        Section(sectionTitle: "Regularly update Scribe data", imageString: "gear", hasToggle: true, sectionState: .matrix)
-      ],
-      hasDynamicData: nil
-    )
-  ]
-}
-
-enum InstallationDataTable {
-  static var installationDataTable = [
-    ParentTableCellModel(
-      headingTitle: "Download language data",
-      section: [
-        Section(sectionTitle: "Add data to scribe keyboards", imageString: "tray", hasToggle: false, sectionState: .downloadData)
-      ],
-      hasDynamicData: nil
-    )
-  ]
-}
+//struct DownloadDataTable {
+//  static var downloadDataTable = [
+//    ParentTableCellModel(
+//      headingTitle: "Data updates",
+//      section: [
+//        Section(sectionTitle: "Check for new data", imageString: "checkmark.icloud", hasToggle: false, sectionState: .checkData),
+//        Section(sectionTitle: "Regularly update Scribe data", imageString: "gear", hasToggle: true, sectionState: .matrix)
+//      ],
+//      hasDynamicData: nil
+//    )
+//  ]
+//}
+//
+//enum InstallationDataTable {
+//  static var installationDataTable = [
+//    ParentTableCellModel(
+//      headingTitle: "Download language data",
+//      section: [
+//        Section(sectionTitle: "Add data to scribe keyboards", imageString: "tray", hasToggle: false, sectionState: .downloadData)
+//      ],
+//      hasDynamicData: nil
+//    )
+//  ]
+//}
