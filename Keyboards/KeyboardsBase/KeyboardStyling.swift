83b61921268303167da138e444069e227d168015
Jag-Marcel <marcelmehl123@gmail.com>
2024-05-28 18:29:00 +0200

    Fixed info icon appearing when switching views
    The info icon would stick around even if a word was available when switching tabs (more info in the comments of the PR)

--- Keyboards/KeyboardsBase/KeyboardStyling.swift
+++ Keyboards/KeyboardsBase/KeyboardStyling.swift
@@ -30,19 +30,25 @@ func styleBtn(btn: UIButton, title: String, radius: CGFloat) {
   btn.layer.masksToBounds = false
   btn.layer.cornerRadius = radius
   btn.setTitle(title, for: .normal)
-  // Displays info button for missing data
-  if title == invalidCommandMsg {
-    if #available(iOS 15.0, *) {
-      var configuration = UIButton.Configuration.plain()
-      configuration.baseForegroundColor = UITraitCollection.current.userInterfaceStyle == .light ? specialKeyColor : keyColor
-      configuration.image = UIImage(systemName: "info.circle.fill")
-      configuration.imagePlacement = .trailing
-      configuration.imagePadding = 3
-      btn.configuration = configuration
+  if #available(iOS 15.0, *) {
+    // Displays info button for missing data
+    if title == invalidCommandMsg {
+      btn.configuration = UIButton.Configuration.plain()
+      btn.configuration?.baseForegroundColor = UITraitCollection.current.userInterfaceStyle == .light ? specialKeyColor : keyColor
+      btn.configuration?.image = UIImage(systemName: "info.circle.fill")
+      btn.configuration?.imagePlacement = .trailing
+      btn.configuration?.imagePadding = 3
     } else {
+      btn.configuration = nil
+    }
+  } else {
+    if title == invalidCommandMsg {
       btn.semanticContentAttribute = .forceLeftToRight // Has to be changed once support for RTL languages is implemented
       btn.setImage(UIImage(systemName: "info.circle.fill"), for: .normal)
       btn.tintColor = UITraitCollection.current.userInterfaceStyle == .light ? specialKeyColor : keyColor
+    } else {
+      btn.semanticContentAttribute = .unspecified
+      btn.setImage(nil, for: .normal)
     }
   }
   btn.contentHorizontalAlignment = UIControl.ContentHorizontalAlignment.center
