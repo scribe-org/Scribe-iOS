8f4b50bc2e3258863c2e1a601581c178aa32297c
Jag-Marcel <marcelmehl123@gmail.com>
2024-06-01 15:47:26 +0200

    Resolved conflicts with main branch
    Mainly resolved conflicts with the main branch and updated some localizations

--- Scribe/AppTexts/InstallScreen.swift
+++ Scribe/AppTexts/InstallScreen.swift
@@ -38,73 +38,28 @@ func getInstallationDirections(fontSize: CGFloat) -> NSMutableAttributedString {
   """, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)])
 
   var settingsLink = NSMutableAttributedString()
-  if #available(iOS 17.2.0, *) {
-    let linkText = NSLocalizedString("install.settingsLink", comment: "Text for the link to the scribe section in the settings")
-    settingsLink = addHyperLinks(
-      originalText: linkText,
-      links: [linkText: "MakeTextLink"], // placeholder as there's a button over it
-      fontSize: fontSize
-    )
+  let linkText = NSLocalizedString("install.settingsLink", comment: "Text for the link to the scribe section in the settings")
+  settingsLink = addHyperLinks(
+    originalText: linkText,
+    links: [linkText: "MakeTextLink"], // placeholder as there's a button over it
+    fontSize: fontSize
+  )
 
-    let installStart = concatAttributedStrings(left: startOfBody, right: settingsLink)
+  let installStart = concatAttributedStrings(left: startOfBody, right: settingsLink)
 
-    let installDirections = NSMutableAttributedString(string: "\n\n2. " + NSLocalizedString("install.text", comment: "Main text for the installation screen"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)])
-    
-    var toAppend = NSMutableAttributedString(string: " " + NSLocalizedString("keyboards", comment: "Keyboards button in the settings"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)])
-    toAppend.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: fontSize), range: NSRange(location: 0, length: toAppend.length))
-    installDirections.append(toAppend)
+  let installDirections = NSMutableAttributedString(string: "\n\n2. " + NSLocalizedString("install.text", comment: "Main text for the installation screen"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)])
 
-    installDirections.append(NSMutableAttributedString(string: "\n\n3. " + NSLocalizedString("install.text2", comment: "Main text for the installation screen 2"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)]))
-    
-    installDirections.append(globeString)
-    
-    installDirections.append(NSMutableAttributedString(string: NSLocalizedString("install.text3", comment: "Main text for the installation screen 3"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)]))
+  var toAppend = NSMutableAttributedString(string: " " + NSLocalizedString("keyboards", comment: "Keyboards button in the settings"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)])
+  toAppend.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: fontSize), range: NSRange(location: 0, length: toAppend.length))
+  installDirections.append(toAppend)
 
-    return concatAttributedStrings(left: installStart, right: installDirections)
-  } else {
-    let linkText = NSLocalizedString("install.settingsLinkOld", comment: "Text for the link to the scribe section in the settings on older iOS versions")
-    settingsLink = addHyperLinks(
-      originalText: linkText,
-      links: [linkText: "MakeTextLink"], // placeholder as there's a button over it
-      fontSize: fontSize
-    )
+  installDirections.append(NSMutableAttributedString(string: "\n\n3. " + NSLocalizedString("install.text2", comment: "Main text for the installation screen 2") + " ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)]))
 
-    let installStart = concatAttributedStrings(left: startOfBody, right: settingsLink)
+  installDirections.append(globeString)
 
-    let installDirections = NSMutableAttributedString(string: "\n\n2. " + NSLocalizedString("install.textOld", comment: "Main text for the installation screen on older iOS versions") + " ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)])
+  installDirections.append(NSMutableAttributedString(string: " " + NSLocalizedString("install.text3", comment: "Main text for the installation screen 3"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)]))
 
-    var toAppend = NSMutableAttributedString(string: NSLocalizedString("settings.general", comment: "General section of system settings"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)])
-    toAppend.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: fontSize), range: NSRange(location: 0, length: toAppend.length))
-    installDirections.append(toAppend)
-
-    installDirections.append(NSMutableAttributedString(string: " " + NSLocalizedString("install.textOld2", comment: "Main text for the installation screen on older iOS versions 2") + ":\n\n      ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)]))
-
-    toAppend = NSMutableAttributedString(string: NSLocalizedString("keyboard", comment: "Keyboard button in the settings") + "\n\n      ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)])
-    toAppend.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: fontSize), range: NSRange(location: 0, length: toAppend.length))
-    installDirections.append(toAppend)
-
-    installDirections.append(arrowString)
-
-    toAppend = NSMutableAttributedString(string: " " + NSLocalizedString("keyboards", comment: "Keyboards button in the settings") + "\n\n            ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)])
-    toAppend.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: fontSize), range: NSRange(location: 0, length: toAppend.length))
-    installDirections.append(toAppend)
-
-    installDirections.append(arrowString)
-
-    installDirections.append(NSMutableAttributedString(string: " " + NSLocalizedString("install.textOld3", comment: "Main text for the installation screen on older iOS versions 3"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)]))
-
-    toAppend = NSMutableAttributedString(string: " Scribe ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)])
-    toAppend.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: fontSize), range: NSRange(location: 1, length: "Scribe".count))
-    installDirections.append(toAppend)
-
-    installDirections.append(NSMutableAttributedString(string: NSLocalizedString("install.textOld4", comment: "Main text for the installation screen on older iOS versions 4"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)]))
-
-    installDirections.append(globeString)
-
-    installDirections.append(NSMutableAttributedString(string: " " + NSLocalizedString("install.textOld5", comment: "Main text for the installation screen on older iOS versions 5"), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)]))
-
-    return concatAttributedStrings(left: installStart, right: installDirections)
-  }
+  return concatAttributedStrings(left: installStart, right: installDirections)
 }
 
 /// Formats and returns the full text for the installation guidelines.
