5f3eb9ea441033789257eb583bd511b5db7ef3c5
Jag-Marcel <marcelmehl123@gmail.com>
2024-06-26 17:39:48 +0200

    Added new TableView under keyboard settings
    + in progress class for the new view

---
+++ Scribe/Components/SelectionView/SelectionViewTemplateViewController.swift
@@ -0,0 +1,66 @@
+/**
+ * This file describes
+ *
+ * Copyright (C) 2024 Scribe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import UIKit
+
+final class SelectionViewTemplateViewController: BaseTableViewController {
+  // MARK: - Properties
+
+  override var dataSet: [ParentTableCellModel] {
+    tableData
+  }
+
+  private var tableData: [ParentTableCellModel] = []
+  private var parentSection: Section?
+
+  // MARK: - Functions
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    tableView.rowHeight = UITableView.automaticDimension
+    tableView.estimatedRowHeight = 250
+    tableView.separatorStyle = .none
+  }
+
+  func configureTable(for tableData: [ParentTableCellModel], parentSection: Section) {
+    self.tableData = tableData
+    self.parentSection = parentSection
+
+    title = parentSection.sectionTitle
+  }
+}
+
+// MARK: - UITableViewDataSource
+
+extension SelectionViewTemplateViewController {
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(
+      withIdentifier: InfoChildTableViewCell.reuseIdentifier,
+      for: indexPath
+    ) as? InfoChildTableViewCell else {
+      fatalError("Failed to dequeue InfoChildTableViewCell.")
+    }
+    cell.parentSection = parentSection
+    cell.configureCell(for: tableData[indexPath.section].section[indexPath.row])
+    cell.backgroundColor = lightWhiteDarkBlackColor
+
+    return cell
+  }
+}
