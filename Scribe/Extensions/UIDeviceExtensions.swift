74be72a461fa6373f28807eb45e4b44bc7e66563
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2024-07-03 23:51:18 +0200

    Fix swift linting errors and update changelog given fix

--- Scribe/Extensions/UIDeviceExtensions.swift
+++ Scribe/Extensions/UIDeviceExtensions.swift
@@ -1,9 +1,21 @@
-//
-//  UIDeviceExtensions.swift
-//  Scribe
-//
-//  Created by Admin on 03.07.2024.
-//
+/**
+ * Checks if the device has a home button or not via safe area checks.
+ *
+ * Copyright (C) 2024 Scribe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import UIKit
 
