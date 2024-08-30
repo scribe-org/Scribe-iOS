215b2913936c389f30e7a9e4332db9c3442098cd
DanielGevaZA <daniel@hostech.co.za>
2024-08-30 11:05:03 +0200

    Update to Temp
    Started to add basic required variables and importing UIKit

--- Keyboards/LanguageKeyboards/Hebrew/HEInterfaceVariablesTemp.swift
+++ Keyboards/LanguageKeyboards/Hebrew/HEInterfaceVariablesTemp.swift
@@ -2,8 +2,19 @@
 //  HEInterfaceVariablesTemp.swift
 //  Scribe
 
+import UIKit
+
+public enum HEKeyboardConstants {
+  static let defaultCurrencyKey = "$"
+  static let currencyKeys = ["$", "€", "£", "¥"]
+
+  // Alternate key vars.
+  static let keysWithAlternates = [""]
+  static let keysWithAlternatesLeft = [""]
+  static let keysWithAlternatesRight = [""]
+}
+
 
 
 //  Created by Daniel Geva on 2024/08/30.
 //
-
