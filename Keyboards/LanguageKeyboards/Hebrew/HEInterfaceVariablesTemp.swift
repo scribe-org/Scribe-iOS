caa11fee04ce4726642e32fbcf30712308451db4
DanielGevaZA <daniel@hostech.co.za>
2024-08-30 11:24:29 +0200

    Updated Temp
    Started to add alternate letters

--- Keyboards/LanguageKeyboards/Hebrew/HEInterfaceVariablesTemp.swift
+++ Keyboards/LanguageKeyboards/Hebrew/HEInterfaceVariablesTemp.swift
@@ -4,17 +4,42 @@
 
 import UIKit
 
-public enum HEKeyboardConstants {
-  static let defaultCurrencyKey = "$"
-  static let currencyKeys = ["$", "€", "£", "¥"]
+public enum HebrewKeyboardConstants {
+  static let defaultCurrencyKey = "₪"
+  static let currencyKeys = ["₪", "€", "£", "$"]
 
   // Alternate key vars.
-  static let keysWithAlternates = [""]
-  static let keysWithAlternatesLeft = [""]
-  static let keysWithAlternatesRight = [""]
+  // Add "sofit" letters here
+  static let keysWithAlternates = ["ך","ח", "נ", "צ", "ק", "ת", "פ", "א"]
+    static let keysWithAlternatesLeft = ["ח", "נ", "פ"]
+    static let keysWithAlternatesRight = ["צ", "ק", "ת"]
+    
+    static let alephAlternateKeys = ["א׳"]
+    static let memAlternateKeys = ["ם"]
+    static let nunAlternateKeys = ["ן"]
+    static let tzadiAlternateKeys = ["ץ"]
+    static let kafAlternateKeys = ["ק"]
+    static let kafSofitAlternateKeys = ["ך"]
+    static let tavAlternateKeys = ["ת"]
+    static let peySofitAlternateKeys = ["פ׳"]
+  
 }
 
-
+struct HebrewKeyboardProvider: KeyboardProviderProtocol {
+  // iPhone keyboard layouts.
+  
+  static func genPhoneLetterKeys() -> [[String]] {
+    return KeyboardBuilder()
+      .addRow(["פ", "ם", "ן", "ו", "ט", "א", "ר", "ק", "delete"])
+      .addRow(["ף", "ך", "ל", "ח", "י", "ע", "כ", "ג", "ד", "ש"])
+      .addRow(["ץ", "ת", "צ", "מ", "נ", "ה", "ב", "ס", "ז"])
+      .addRow(["123", "selectKeyboard", "space", "return"]) // "undo", "accent"
+      .build()
+  }
+  
+  // add rest of iphone keyboards here
+  
+}
 
 //  Created by Daniel Geva on 2024/08/30.
 //
