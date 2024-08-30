5f6243aaf91f41205191df449298a048011c9311
DanielGevaZA <daniel@hostech.co.za>
2024-08-30 15:01:33 +0200

    Update to HEInterfaceVariablesTemp
    Added all the iPad and Larger iPad keyboards with correct signs and button labels

--- Keyboards/LanguageKeyboards/Hebrew/HEInterfaceVariablesTemp.swift
+++ Keyboards/LanguageKeyboards/Hebrew/HEInterfaceVariablesTemp.swift
@@ -64,10 +64,67 @@ struct HebrewKeyboardProvider: KeyboardProviderProtocol {
     }
   }
   
+  // iPad keyboard layouts.
+   static func genPadLetterKeys() -> [[String]] {
+     return KeyboardBuilder()
+       .addRow(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "+"])
+       .addRow([",", ".", "פ", "ם", "ן", "ו", "ט", "א", "ר", "ק", "❌"])
+       .addRow(["ף", "ך", "ל", "ח", "י", "ע", "כ", "ג", "ד", "ש"])
+       .addRow(["ץ", "ת", "צ", "מ", "נ", "ה", "ב", "ס", "ז", "הכנס"])
+       .addRow(["selectKeyboard", ".?123", "רווח", ".?123", "hideKeyboard"])
+       .build()
+   }
+
+   static func genPadNumberKeys(currencyKey: String) -> [[String]] {
+     return KeyboardBuilder()
+       .addRow(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "❌"])
+       .addRow(["!", "@", "#", "&", "_", "-", "'", "\"", "(", ")", "הכנס"])
+       .addRow(["#+=", "%", "...", "&", ";", ":", "=", "+", "/", "?", "#+="])
+       .addRow(["selectKeyboard", "אבג", "רווח", "אבג", "hideKeyboard"])
+       // .replaceKey(row: 1, column: 4, to: currencyKey)
+       .build()
+   }
+
+   static func genPadSymbolKeys(currencyKeys: [String]) -> [[String]] {
+     let keyboardBuilder = KeyboardBuilder()
+       .addRow(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "*", "❌"])
+       .addRow(["^", "€", "$", "£", "[", "]", "'", "\"", "<", ">", "הכנס"])
+       .addRow(["123", "§", "|", "~", "*", "·", "{", "}", "\\", "~", "123"])
+       .addRow(["selectKeyboard", "אבג", "רווח", "אבג", "hideKeyboard"])
+
+     if currencyKeys.count < 3 {
+       return keyboardBuilder.build()
+     } else {
+       return keyboardBuilder
+         .replaceKey(row: 1, column: 1, to: currencyKeys[0])
+         .replaceKey(row: 1, column: 2, to: currencyKeys[1])
+         .replaceKey(row: 1, column: 3, to: currencyKeys[2])
+         .build()
+     }
+   }
   
-  // add rest of iphone keyboards here
-  
-}
+  // Expanded iPad keyboard layouts for wider devices.
+    static func genPadExpandedLetterKeys() -> [[String]] {
+      return KeyboardBuilder()
+        .addRow(["§", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "❌"])
+        .addRow([SpecialKeys.indent, "/", "'", "פ", "ם", "ן", "ו", "ט", "א", "ר", "ק", "[", "]", "+"])
+        .addRow([SpecialKeys.capsLock, "ף", "ך", "ל", "ח", "י", "ע", "כ", "ג", "ד", "ש", ",", "\"", "הכנס"])
+        .addRow(["⇧", ";", "ץ", "ת", "צ", "מ", "נ", "ה", "ב", "ס", "ז", ".", "⇧"])
+        .addRow(["selectKeyboard", ".?123", "רווח", ".?123", "hideKeyboard"])
+        .build()
+    }
+
+    static func genPadExpandedSymbolKeys() -> [[String]] {
+      return KeyboardBuilder()
+        .addRow(["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "<", ">", "❌"])
+        .addRow([SpecialKeys.indent, "[", "]", "{", "}", "#", "%", "^", "*", "+", "=", "\"", "|", "—"])
+        .addRow([SpecialKeys.capsLock, "°", "/", ":", ";", "(", ")", "$", "&", "@", "£", "¥", "€", "הכנס"])
+        .addRow(["⇧", "…", "?", "!", "~", "≠", "'", "\"", "_", ",", ".", "-", "⇧"])
+        .addRow(["selectKeyboard", "אבג", "רווח", "אבג", "hideKeyboard"])
+        .build()
+    }
+  }
+
 
 //  Created by Daniel Geva on 2024/08/30.
 //
