01dbdad13187fddd0a4ff6abc8176b7b4373b1e6
henrikth93 <106680402+henrikth93@users.noreply.github.com>
2025-12-27 23:37:32 +0100

    Add word-for-word delete function (#553)
    * Add word-for-word delete function
    
    This commit will add word-for-word delete function if you hold the delete button.
    
    * Fix lint issues
    
    This commit will fix some lint issues
    
    * Fix lint issues
    
    This commit will fix lint issues
    
    * Update changelog
    
    This commit will update the changelog to include the issue 301
    
    * Rename settings variable suffix and update changelog formatting
    
    ---------
    
    Co-authored-by: Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>

--- Scribe/Views/Cells/InfoChildTableViewCell/InfoChildTableViewCell.swift
+++ Scribe/Views/Cells/InfoChildTableViewCell/InfoChildTableViewCell.swift
@@ -147,6 +147,10 @@ final class InfoChildTableViewCell: UITableViewCell {
       let dictionaryKey = languageCode + "EmojiAutosuggest"
       userDefaults.setValue(toggleSwitch.isOn, forKey: dictionaryKey)
 
+    case .toggleWordForWordDeletion:
+      let dictionaryKey = languageCode + "WordForWordDeletion"
+      userDefaults.setValue(toggleSwitch.isOn, forKey: dictionaryKey)
+
     case .none: break
     }
 
@@ -185,6 +189,14 @@ final class InfoChildTableViewCell: UITableViewCell {
         toggleSwitch.isOn = toggleValue
       } else {
         toggleSwitch.isOn = true  // Default value
+      }
+
+    case .toggleWordForWordDeletion:
+      let dictionaryKey = languageCode + "WordForWordDeletion"
+      if let toggleValue = userDefaults.object(forKey: dictionaryKey) as? Bool {
+        toggleSwitch.isOn = toggleValue
+      } else {
+        toggleSwitch.isOn = false // Default value
       }
 
     case .none: break
