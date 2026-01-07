8ce54b6a6c2a6e99d2e35a554e672b9c74ba57ec
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2026-01-08 00:59:40 +0100

    Minor comment edits

--- Scribe/AppDelegate.swift
+++ Scribe/AppDelegate.swift
@@ -27,9 +27,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       UITabBar.appearance().standardAppearance = appearance
       UITabBar.appearance().scrollEdgeAppearance = appearance
     } else {
-      // Fallback for earlier versions: non-transparent
       UITabBar.appearance().isTranslucent = false
-      UITabBar.appearance().barTintColor = .white // Default non-transparent color
+      UITabBar.appearance().barTintColor = .white // default non-transparent color
     }
 
     return true
