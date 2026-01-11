655bb6be0e92422a0b1bb379d6f1f5d3b0438572
Jatin Foujdar <jatin321001@gmail.com>
2026-01-11 23:26:14 +0530

    style: fix swiftlint trailing whitespace violations in tests

--- Tests/Keyboards/KeyboardsBase/EmojiQueryTests.swift
+++ Tests/Keyboards/KeyboardsBase/EmojiQueryTests.swift
@@ -5,21 +5,21 @@ import Foundation
 import XCTest
 
 class EmojiQueryTests: XCTestCase {
-    
+
     func testQueryEmojisPatternMatchingWithCommonKeyword() {
         // This test assumes the database is populated with some emojis.
         // If not, it might return empty, which we also handle.
         let keyword = "happ"
         let results = LanguageDBManager.shared.queryEmojisPatternMatching(of: keyword)
-        
+
         XCTAssertEqual(results.count, 3, "Should always return 3 elements (including empty strings)")
     }
-    
+
     func testQueryEmojisPatternMatchingWithEmptyKeyword() {
         let results = LanguageDBManager.shared.queryEmojisPatternMatching(of: "")
         XCTAssertEqual(results.count, 3)
     }
-    
+
     func testQueryEmojisPatternMatchingWithNonExistentKeyword() {
         let results = LanguageDBManager.shared.queryEmojisPatternMatching(of: "nonexistentkeyword12345")
         XCTAssertEqual(results.count, 3)
--- Tests/Keyboards/KeyboardsBase/KeyboardCommandTests.swift
+++ Tests/Keyboards/KeyboardsBase/KeyboardCommandTests.swift
@@ -5,13 +5,13 @@ import Foundation
 import XCTest
 
 class KeyboardCommandTests: XCTestCase {
-    
+
     func testColonToEmojiIsEnabled() {
         let keyboard = KeyboardViewController()
         // Default should be true as per the implementation
         XCTAssertTrue(keyboard.colonToEmojiIsEnabled())
     }
-    
+
     // Note: Testing UI state transitions often requires a more complex setup
     // with UIInputViewController and its proxy. Here we test the logic we can.
 }
