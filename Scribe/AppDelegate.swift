01cfdba456874ecbbe591c5a08dbd20bba63116e
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2026-01-15 21:32:29 +0100

    Minor comment update

--- Scribe/AppDelegate.swift
+++ Scribe/AppDelegate.swift
@@ -67,7 +67,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         tabBarController.selectedIndex = 0
       }
 
-      // Pop to root in Installation tab's navigation controller
+      // Pop to root in Installation tab's navigation controller.
       if let navController = tabBarController.viewControllers?[0] as? UINavigationController {
         navController.popToRootViewController(animated: false)
       }
