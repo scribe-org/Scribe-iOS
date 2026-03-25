ee7f22d88af03ed8eb8f13f5d6d6e1f8aa0169ba
Purnama S Rahayu <purnamasrahayu@gmail.com>
2026-03-25 18:46:19 +0700

    fix build error after InformationToolTip merge conflict

--- Keyboards/KeyboardsBase/ToolTip/Model/InformationToolTipData.swift
+++ Keyboards/KeyboardsBase/ToolTip/Model/InformationToolTipData.swift
@@ -34,6 +34,33 @@ enum InformationToolTipData {
         ]
     )
 
+    static let wiktionaryExplanation = NSMutableAttributedString(
+        string: invalidCommandTextWiktionary1,
+        attributes: [
+            NSAttributedString.Key.font: UIFont.systemFont(
+                ofSize: DeviceType.isPhone ? letterKeyWidth / 2 : letterKeyWidth / 2.5
+            )
+        ]
+    )
+
+    static let wiktionaryTranslationOrigin = NSMutableAttributedString(
+        string: invalidCommandTextWiktionary2,
+        attributes: [
+            NSAttributedString.Key.font: UIFont.systemFont(
+                ofSize: DeviceType.isPhone ? letterKeyWidth / 2 : letterKeyWidth / 2.5
+            )
+        ]
+    )
+
+    static let howToContributeWiktionary = NSMutableAttributedString(
+        string: invalidCommandTextWiktionary3,
+        attributes: [
+            NSAttributedString.Key.font: UIFont.systemFont(
+                ofSize: DeviceType.isPhone ? letterKeyWidth / 2 : letterKeyWidth / 2.5
+            )
+        ]
+    )
+
     static func getContent() -> [NSMutableAttributedString] {
         [wikiDataExplanation, wikiDataContationOrigin, howToContribute]
     }
