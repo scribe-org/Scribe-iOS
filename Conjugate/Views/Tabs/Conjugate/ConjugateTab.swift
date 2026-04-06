75f47ac88b27e871a01d42f78d0df5588d15d29b
Shabicha Sureshkumar <shabichasureshkumar@gmail.com>
2026-04-06 13:55:42 -0400

    feat: add Conjugate tab download data menu option (SwiftUI)

--- Conjugate/Views/Tabs/Conjugate/ConjugateTab.swift
+++ Conjugate/Views/Tabs/Conjugate/ConjugateTab.swift
@@ -4,10 +4,20 @@ import SwiftUI
 
 struct ConjugateTab: View {
     var body: some View {
-        AppNavigation {
-            Text("Conjugate")
-                .font(.largeTitle)
-                .navigationTitle("Conjugate")
-        }
+      AppNavigation {
+            ScrollView {
+              VStack(spacing: 20) {
+                Image("ScribeLogo")
+                  .resizable()
+                  .scaledToFit()
+                  .frame(width: 200, height: 100)
+                  .padding(.top, 30)
+                ConjugateDataDownload()
+              }
+              .padding()
+            }
+            .background(Color("scribeAppBackground"))
+            .navigationBarHidden(true)
+          }
     }
 }
