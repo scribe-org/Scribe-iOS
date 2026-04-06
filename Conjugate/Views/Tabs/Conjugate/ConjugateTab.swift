ef016b916420119d01602f7e9093ae93371f1215
Shabicha Sureshkumar <shabichasureshkumar@gmail.com>
2026-04-06 14:57:47 -0400

    feat: reuse CardView from keyboard app for Conjugate tab download option

--- Conjugate/Views/Tabs/Conjugate/ConjugateTab.swift
+++ Conjugate/Views/Tabs/Conjugate/ConjugateTab.swift
@@ -12,12 +12,30 @@ struct ConjugateTab: View {
                   .scaledToFit()
                   .frame(width: 200, height: 100)
                   .padding(.top, 30)
-                ConjugateDataDownload()
-              }
-              .padding()
-            }
-            .background(Color("scribeAppBackground"))
-            .navigationBarHidden(true)
-          }
-    }
-}
+                CardView(
+                            title: NSLocalizedString(
+                              "i18n.app.download.menu_option.conjugate_title",
+                              value: "Verb data",
+                              comment: ""
+                            ),
+                            mainText: NSLocalizedString(
+                              "i18n.app.download.menu_option.conjugate_download_data_start",
+                              value: "Download data to start conjugating!",
+                              comment: ""
+                            ),
+                            subtitle: NSLocalizedString(
+                              "i18n.app.download.menu_option.conjugate_description",
+                              value: "Add new data to Scribe Conjugate.",
+                              comment: ""
+                            )
+                          ) {
+                            // Insert navigation to download screen.
+                          }
+                        }
+                        .padding()
+                      }
+                      .background(Color("scribeAppBackground"))
+                      .navigationBarHidden(true)
+                    }
+                  }
+                }
