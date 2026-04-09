83caeea06a604d833fbc2fc4c55d5fca7b8175f3
Shabicha <107499439+shabicha@users.noreply.github.com>
2026-04-08 20:03:41 -0400

    Add Conjugate tab to Conjugate app with download data menu option (#646)
    * feat: add Conjugate tab download data menu option (SwiftUI)
    
    * style: comment formatting
    
    * feat: reuse CardView from keyboard app for Conjugate tab download option
    
    ---------
    
    Co-authored-by: Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>

--- Conjugate/Views/Tabs/Conjugate/ConjugateTab.swift
+++ Conjugate/Views/Tabs/Conjugate/ConjugateTab.swift
@@ -4,10 +4,38 @@ import SwiftUI
 
 struct ConjugateTab: View {
     var body: some View {
-        AppNavigation {
-            Text("Conjugate")
-                .font(.largeTitle)
-                .navigationTitle("Conjugate")
-        }
-    }
-}
+      AppNavigation {
+            ScrollView {
+              VStack(spacing: 20) {
+                Image("ScribeLogo")
+                  .resizable()
+                  .scaledToFit()
+                  .frame(width: 200, height: 100)
+                  .padding(.top, 30)
+                CardView(
+                            title: NSLocalizedString(
+                              "i18n.app.download.menu_option.conjugate_title",
+                              value: "Verb data",
+                              comment: ""
+                            ),
+                            mainText: NSLocalizedString(
+                              "i18n.app.download.menu_option.conjugate_download_data_start",
+                              value: "Download data to start conjugating!",
+                              comment: ""
+                            ),
+                            subtitle: NSLocalizedString(
+                              "i18n.app.download.menu_option.conjugate_description",
+                              value: "Add new data to Scribe Conjugate.",
+                              comment: ""
+                            )
+                          ) {
+                            // Insert navigation to download screen.
+                          }
+                        }
+                        .padding()
+                      }
+                      .background(Color("scribeAppBackground"))
+                      .navigationBarHidden(true)
+                    }
+                  }
+                }
