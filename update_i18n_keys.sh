e265deafaefa3f189a278a013af4672c1390e1c4
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2026-07-11 18:52:21 +0200

    Update i18n script commands

--- update_i18n_keys.sh
+++ update_i18n_keys.sh
@@ -1,5 +1,7 @@
 # Run this script to update the Scribe-i18n keys in the project.
-# macOS: sh update_i18n_keys.sh
+# macOS: ./update_i18n_keys.sh
+# Linux: ./update_i18n_keys.sh
+# Windows: Run the command below.
 cd Scribe/i18n
 git pull origin main
 cd ../..
